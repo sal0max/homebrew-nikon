c330a4ed7547f8d845301d3216bfae2b36d4d257
Max <msal.coding@gmail.com>
2021-03-05 14:57:02 +0100

    update camera-control-pro-2 from 2.33.0 to 2.33.1

--- Casks/camera-control-pro-2.rb
+++ Casks/camera-control-pro-2.rb
@@ -1,10 +1,10 @@
 cask "camera-control-pro-2" do
-  version "2.33.0"
-  sha256 "1e06e4cbb797e9265e564b707e5e910f0f312965fa1c85577eac22af2ca05c05"
+  version "2.33.1"
+  sha256 "b27b7f07ba138cfaf65f5bb2fd04122f500bb44cf7d826204ca3a20ce236632b"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/181.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/191.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
