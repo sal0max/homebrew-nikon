a1a29718f8d7bca812f698ad5f20760c7b0a1d35
Max <msal.coding@gmail.com>
2021-03-05 15:01:56 +0100

    update viewnx-i from 1.4.4 to 1.4.5

--- Casks/viewnx-i.rb
+++ Casks/viewnx-i.rb
@@ -1,10 +1,10 @@
 cask "viewnx-i" do
-  version "1.4.4"
-  sha256 "576986851ddbe55c2a67769d9bfcc8ba2da4f978edc70456357378562cde6732"
+  version "1.4.5"
+  sha256 "bf50551153c4cec53e9e352560a5d638826a18fd92fc66fbdc23191b4374ed3a"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/183.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/193.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
