13028ca8b1147f4192b410361f1a809c89f41328
Max <msal.coding@gmail.com>
2021-03-05 14:59:20 +0100

    update capture-nx-d from 1.6.4 to 1.6.5

--- Casks/capture-nx-d.rb
+++ Casks/capture-nx-d.rb
@@ -1,10 +1,10 @@
 cask "capture-nx-d" do
-  version "1.6.4"
-  sha256 "62c0cb3e8766729e28320043ac20df02e10827c0bce68fc7a46bdb5f685e0d0c"
+  version "1.6.5"
+  sha256 "bf50551153c4cec53e9e352560a5d638826a18fd92fc66fbdc23191b4374ed3a"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/182.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/194.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
