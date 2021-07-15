68347dc0cf88485445a672fb0a569c15da9dd6c6
Max <msal.coding@gmail.com>
2021-07-15 15:16:01 +0200

    update picture-control-utility-2 to 2.4.8

--- Casks/picture-control-utility-2.rb
+++ Casks/picture-control-utility-2.rb
@@ -1,10 +1,10 @@
 cask "picture-control-utility-2" do
-  version "2.4.7"
-  sha256 "823b8434ade3cbb19cf29a2c7ab499802369db7be48498b8bd0fb0033b86c809"
+  version "2.4.8"
+  sha256 "ddbf60f6e7228026c7b2b7191ee6a5289d7bd4aebe607fdedaa84bac251d082e"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/180.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/200.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
