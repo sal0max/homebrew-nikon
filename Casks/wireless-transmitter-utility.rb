899b1de2a4f3ac75da03c6651f3074ccbd30c951
Max <msal.coding@gmail.com>
2021-07-15 15:24:29 +0200

    update wireless-transmitter-utility to 1.9.5

--- Casks/wireless-transmitter-utility.rb
+++ Casks/wireless-transmitter-utility.rb
@@ -1,10 +1,10 @@
 cask "wireless-transmitter-utility" do
-  version "1.9.4"
-  sha256 "4aebddf0a8f1e2bf535ce1712310c56aeec14db075ea073f5f2c8b792c819823"
+  version "1.9.5"
+  sha256 "79059c363d89e3fd844b9a14d74edf55138eff1280fa75877f8697322e4006d6"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/179.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/198.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
