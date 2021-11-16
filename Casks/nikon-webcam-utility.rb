32fdc9e05632cc7ece0b32c6c5604790e45bac07
Max <msal.coding@gmail.com>
2021-11-16 11:57:00 +0100

    update nikon-webcam-utility to 1.1.0

--- Casks/nikon-webcam-utility.rb
+++ Casks/nikon-webcam-utility.rb
@@ -1,10 +1,10 @@
 cask "nikon-webcam-utility" do
-  version "1.0.1_1"
-  sha256 "59d5eba98ab8fec975e10bff2c2d420bddd1a75bcd2d381d43686737233a2ca8"
+  version "1.1.0"
+  sha256 "efd468bb1e213828e9d957be3c139a83e12532530df2a0e98ea830df62ab3c35"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/188.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/209.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
