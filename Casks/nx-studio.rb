f4242a4dc98bfdd12e92a3bb2669f12e3b53d9b1
Maximilian Salomon <msal.coding@gmail.com>
2023-08-03 16:52:49 +0200

    update nx-studio

--- Casks/nx-studio.rb
+++ Casks/nx-studio.rb
@@ -1,10 +1,10 @@
 cask "nx-studio" do
-  version "1.4.0"
-  sha256 "3445f037543ad1e4e64c30d93bfe9511d2cd97aba81403eb9c40aa72bee08c7a"
+  version "1.4.1"
+  sha256 "b8f6ab645808dcbbd9a841d5d0c02f5bb8374b988ef1a2169dcf8c90670e93ae"
 
   url do
     require "open-uri"
-    URI("https://downloadcenter.nikonimglib.com/en/download/sw/242.html").open do |landing_page|
+    URI("https://downloadcenter.nikonimglib.com/en/download/sw/243.html").open do |landing_page|
       content = landing_page.read
       content.scan(/id="for64".*href="(.*)".*\.dmg/m)
              .flatten
